aa810bb92655f74dcfc9317a7f6edb903fc32f5b
Ivan Volodartsev <untrupus@gmail.com>
2020-10-22 23:24:58 +0600

    create structure

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
+const categories = require("./app/categories");
+const items = require("./app/items");
+const places = require("./app/places");
 const port = 8000;
+
+app.use(express.json());
+app.use(express.static('public'));
+app.use("/categories", categories);
+app.use("/items", items);
+app.use("/places", places);
 
 app.listen(port, () => {
     console.log(`Server started on ${port} port!`);
